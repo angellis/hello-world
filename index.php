f63b90e37f29f75e86e33c6b8841997427c7851b
Ang <angie@ridge-creative.com>
2017-09-12 20:38:11 -0600

    Added portfolio index.php

---
+++ index.php
@@ -0,0 +1,285 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+    <title>Angie Ellis</title>
+      
+            
+    <link rel="stylesheet" href="css/meyerreset.css">
+    <link rel="stylesheet" href="css/main.css">
+<!--typekit fonts      -->
+    <script src="https://use.typekit.net/ezt6hts.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!--jQuery      -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+      <script src="js/jquery-1.12.4.min.js"></script>
+      
+<!-- favicons     -->
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />      
+<!--fontawesome embed code      -->
+      <script src="https://use.fontawesome.com/de2e8815a3.js"></script>     
+
+
+  </head>
+    
+  <body class="map">
+    <div id="wrapper">
+          
+     <div class="grid"> 
+         <header>
+            <div class="col-1-1 nopad">
+               <a href="index.php">
+                   <img id="logo" src="img/logomark-ae.svg" alt="Angie Ellis Triangle Logo">
+                   <h3 class="logoname">Angie Ellis</h3>
+               </a>
+               
+            </div>
+
+            <nav class="col-1-1 nopad stroke">
+              <ul>
+                <li><a href="index.php" class="current-nav">Work</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="about.php">About</a></li>
+              </ul>
+            </nav>
+        </header>
+    <div class="side-space-outer">    
+        <main>
+            
+          <div class="normal-side-space">
+                    <!-- Changing word code from http://stackoverflow.com/questions/32445323/jquery-replace-text-every-2-seconds-with-words-from-an-array -->
+          <div class="col-1-1 nopad hero-area">
+                <div class="hero">Designer <span class="taupe">&#124; </span></div>
+                <div class="hero hero2 taupe " id="word">Developer</div>
+          </div>  
+            <div class="col-1-1 nopad hero-text"> 
+                <h4>I&rsquo;m an experienced designer who codes, and I&rsquo;m looking for a team to create something greater than any of us can achieve on our own. I specialized in print before immersing myself in web design. I&rsquo;m strategic, organized and embrace brand consistency. </h4>
+            </div>
+            <div class="col-1-1 nopad">
+                <img class="h1arrow" src="img/arrow-sm.png" alt="arrow pointing down"><h1>Work</h1>
+            </div>
+          </div>  <!--normal side space-->
+           
+      <div id="side-space-inner">
+                    <!--Code for text showing on hover adapted from http://css-workshop.com/hover-box-text-over-images-on-hover-and-more/-->
+
+        <div class="grid"> 
+   
+
+
+          <div class="col-1-3 nopad">
+                <a href="workprofessorsedge.php">  
+                   <div class="hvrbox">
+                        <img src="img/grid-pe.jpg" alt="Professor's Edge detail of home page" class="hvrbox-layer_bottom">
+                        <div class="hvrbox-layer_top">
+                            <div class="hvrbox-text">
+                                <h4>Concept</h4>
+                                <h4>Branding</h4>
+                                <h4>Design</h4>
+                                <h4>UX &#124; UI</h4>
+                                <h4>Front-End</h4>
+                            </div>
+                        </div>
+                    </div> <!--hvrbox-->
+                    <div class="project-block">
+                        <h4>Concept . Branding . Design . Code</h4>
+                    </div>
+              </a>
+            </div> <!--col-1-3-->
+
+            <div class="col-1-3 nopad">
+              <a href="workaauw.php">  
+
+                    <div class="hvrbox">
+                        <img src="img/grid-aauw.jpg" alt="AAUW detail of home page" class="hvrbox-layer_bottom">
+                        <div class="hvrbox-layer_top">
+                            <div class="hvrbox-text">
+                                <h4>Design</h4>
+                                <h4>UX &#124; UI</h4>
+                                <h4>Front-End</h4>
+                            </div>
+                        </div>
+                    </div> <!--hvrbox-->
+                   <div class="project-block">
+                        <h4>Design . Code</h4>
+                    </div>
+                </a>
+            </div> <!--col-1-3-->
+
+            <div class="col-1-3 nopad">
+               <a href="workamigos.php">                  
+                    <div class="hvrbox">
+                        <img src="img/grid-amigos.jpg" alt="Casa de Amigos detail of home page" class="hvrbox-layer_bottom">
+                        <div class="hvrbox-layer_top">
+                            <div class="hvrbox-text">
+                                <h4>Branding</h4>
+                                <h4>Design</h4>
+                                <h4>UX &#124; UI</h4>
+                                <h4>Front-End</h4>
+                            </div>
+                        </div>
+                    </div> <!--hvrbox-->
+                    <div class="project-block">
+                        <h4 class="mobileonly">Branding . Design . Code</h4>
+                    </div>
+                </a>
+            </div> <!--col-1-3-->
+    
+           
+
+
+             <div class="col-1-3 nopad">
+                <div class="hvrbox">
+                 <a href="workmarketnine.php">                  
+                    <img src="img/grid-marketnine.jpg" alt="Market Nine detail of web page" class="hvrbox-layer_bottom">
+                    <div class="hvrbox-layer_top">
+                        <div class="hvrbox-text">
+                            <h4>Branding</h4>
+                            <h4>Design</h4>
+                            <h4>UX &#124; UI</h4>
+                            <h4>Front-End</h4>
+                        </div>
+                    </div>
+                 </a>
+                </div> <!--hvrbox-->
+
+                <div class="project-block">
+                    <h4 class="mobileonly">Branding . Design . Code</h4>
+                </div>
+
+            </div> <!--col-1-3-->
+
+            <div class="col-1-3 nopad">
+                <div class="hvrbox">
+                <a href="worktype.php">                  
+                   <img src="img/grid-type.jpg" alt="detail of typography" class="hvrbox-layer_bottom">
+                    <div class="hvrbox-layer_top">
+                        <div class="hvrbox-text">
+                            <h4>Typography</h4>
+                            <h4>Print Design</h4>
+                        </div>
+                    </div>
+                 </a>
+                </div> <!--hvrbox-->
+               <div class="project-block">
+                    <h4 class="mobileonly">Typography . Design . Print</h4>
+                </div>
+              
+            </div> <!--col-1-3-->
+
+            <div class="col-1-3 nopad">
+                <div class="hvrbox">
+                 <a href="workvast.php">                  
+                    <img src="img/grid-vast.jpg" alt="Vast private equity dedicated to the vast opportunity of women entrepreneurs" class="hvrbox-layer_bottom">
+                    <div class="hvrbox-layer_top">
+                        <div class="hvrbox-text">
+                            <h4>Naming</h4>
+                            <h4>Branding</h4>
+                            <h4>Print Design</h4>
+                        </div>
+                    </div>
+                 </a>
+                </div> <!--hvrbox-->
+               <div class="project-block">
+                    <h4 class="mobileonly">Branding . Design . Print</h4>
+                </div>
+
+            </div> <!--col-1-3-->
+
+         
+            
+
+
+             <div class="col-1-3 nopad">
+               <a href="workironbison.php">  
+                    <div class="hvrbox">
+                        <img src="img/grid-ironbison.jpg" alt="iron bison logo" class="hvrbox-layer_bottom">
+                        <div class="hvrbox-layer_top">
+                            <div class="hvrbox-text">
+                                <h4>Naming</h4>
+                                <h4>Branding</h4>
+                                <h4>Print Design</h4>
+                            </div>
+                        </div>
+                    </div> <!--hvrbox-->
+                      <div class="project-block">
+                        <h4 class="mobileonly">Branding . Design . Print</h4>
+                    </div>
+               </a>
+            </div> <!--col-1-3-->
+        
+
+            <div class="col-1-3 nopad">
+              <a href="workequality.php">  
+                <div class="hvrbox">
+                    <img src="img/grid-eu.jpg" alt="desert sunset illustration" class="hvrbox-layer_bottom">
+                    <div class="hvrbox-layer_top">
+                        <div class="hvrbox-text">
+                            <h4>Branding</h4>
+                            <h4>Print Design</h4>
+                        </div>
+                    </div>
+                </div> <!--hvrbox-->
+                <div class="project-block">
+                    <h4 class="mobileonly">Branding . Design . Print</h4>
+                </div>
+               </a>
+            </div> <!--col-1-3-->
+
+            <div class="col-1-3 nopad">
+              <a href="workspa.php">
+                <div class="hvrbox">
+                    <img src="img/grid-spa.jpg" alt="rustler spa brochure" class="hvrbox-layer_bottom">
+                    <div class="hvrbox-layer_top">
+                        <div class="hvrbox-text">
+                            <h4>Branding</h4>
+                            <h4>Print Design</h4>
+                        </div>
+                    </div>
+                </div> <!--hvrbox-->
+                 <div class="project-block">
+                    <h4 class="mobileonly">Branding . Design . Print</h4>
+                </div>
+             </a>
+            </div> <!--col-1-3-->
+        </div>  <!--grid-->
+    </div>          <!--#side-space-->
+            
+    <div class="normal-side-space">
+       
+        <?php include("footer.php"); ?>
+        
+    </div>  <!--normal side space-->
+
+      
+        </main>
+
+        </div>  <!--side space outer-->
+     </div><!--  grid-->
+   </div><!--  wrapper-->
+<!--retina.js-->
+      <script type="text/javascript" src="js/retina.min.js"></script> 
+  </body>
+</html>
+
+
+<!--// code adapted from http://stackoverflow.com/questions/32445323/jquery-replace-text-every-2-seconds-with-words-from-an-array-->
+
+
+<script>
+$(function () {
+  count = 0;
+  wordsArray = ["Developer", "Project Manager", "Team Leader", "Editor", "Writer", "UX-UI"];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(400, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
+    });
+  }, 2500);
+});
+    
+</script>
